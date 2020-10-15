0328fbbdd776b7315faeba2a1ba1b891c9448412
Michael Klepac <mklepac90@gmail.com>
2020-10-15 03:28:19 +0000

    added dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["BASIC_AUTHENTICATION_USER"], password: ENV["BASIC_AUTHENTICATION_PASSWORD"]
 
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
