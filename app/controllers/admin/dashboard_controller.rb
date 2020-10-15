db492c1172d56f61ca805ad5b8c84f6902a2224b
Michael Klepac <mklepac90@gmail.com>
2020-10-15 03:06:04 +0000

    added auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["BASIC_AUTHENTICATION_USER"], password: ENV["BASIC_AUTHENTICATION_PASSWORD"]
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["BASIC_AUTHENTICATION_USER"], password: ENV["BASIC_AUTHENTICATION_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
